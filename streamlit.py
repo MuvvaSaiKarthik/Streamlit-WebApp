0b6cf429a6b4532426314ac68d1989878f7330c4
MuvvaSaiKarthik <54205485+MuvvaSaiKarthik@users.noreply.github.com>
2024-01-06 13:05:47 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -3,7 +3,9 @@ import streamlit as st
 
 st.title('Dashboard')
 
-security_data = pd.read_csv('E:\\Assignment\\Input\\securities.csv')
+# security_data = pd.read_csv('E:\\Assignment\\Input\\securities.csv')
+security_data = pd.read_csv('E:\\Assignment\\Input\\new_values\\fo23FEB2023bhav.csv')
+
 
 st.dataframe(security_data)
 
